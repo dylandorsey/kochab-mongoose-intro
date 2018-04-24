15ffd00aa5ad31f01a8be4531780d32c16049b07
Dylan Dorsey <dylan.dorsey@gmail.com>
2018-04-24 13:53:21 -0500

    library.router.js contains book array

--- server/routes/library.router.js
+++ server/routes/library.router.js
@@ -1,8 +1,42 @@
 const express = require('express');
 const router = express.Router();
+const books = [
+    {
+        title: "A Tale of Two Cities",
+        author: "Charles Dickens",
+        edition: 2,
+    },
+    {
+        title: "Murder on the Orient Express",
+        author: "Agatha Christie",
+        edition: 2
+    },
+    {
+        title: "Snow Crash",
+        author: "Neal Steve",
+        edition: 1.2,
+        ratings: [
+            { score: 5 },
+            { score: 1 }
+        ]
+    },
+    {
+        title: "Catcher in the Rye",
+        author: "Someone",
+        edition: 1,
+        ratings: [
+            { score: 2 },
+            { score: 5 },
+            { score: 3.5 },
+            { score: 4 }
+        ]
+    }
+]
 
-router.get('/', (req,res) => {
+router.get('/', (req, res) => {
     console.log('meow');
+    res.send(books)
 });
 
 module.exports = router;
+
